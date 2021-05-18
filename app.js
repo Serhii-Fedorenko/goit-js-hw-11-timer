244e2cd8a7d376a941826abfa3494ecf6d761067
serhii.fedorenko90@gmail.com <serhii.fedorenko90@gmail.com>
2021-05-18 19:57:59 +0300

    done

--- app.js
+++ app.js
@@ -1,13 +1,51 @@
 class CountdownTimer {
-    constructor({ selector, targetDate }) {
-      this.selector = selector;
-      this.targetDate = targetDate;
-      this.refs = {
-        days: document.querySelector(`[data-value="days"]`),
-        hours: document.querySelector(`[data-value="hours"]`),
-        mins: document.querySelector(`[data-value="mins"]`),
-        secs: document.querySelector(`[data-value="secs"]`),
-        timerFace: document.querySelector('#timer-1'),
-      };
-    }
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+    this.refs = {
+      days: document.querySelector(`${selector} [data-value="days"]`),
+      hours: document.querySelector(`${selector} [data-value="hours"]`),
+      mins: document.querySelector(`${selector} [data-value="mins"]`),
+      secs: document.querySelector(`${selector} [data-value="secs"]`),
+      timerFace: document.querySelector("#timer-1"),
+    };
+    this.renderingTimer();
+  }
+
+  renderingTimer() {
+    setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = this.targetDate - currentTime;
+      this.updateClockface(this.getTimeComponents(deltaTime));
+    }, 1000);
+  }
+
+  updateClockface({ days, hours, mins, secs }) {
+    this.refs.days.innerHTML = days;
+    this.refs.hours.innerHTML = hours;
+    this.refs.mins.innerHTML = mins;
+    this.refs.secs.innerHTML = secs;
+  }
+
+  getTimeComponents(time) {
+    const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+
+    const hours = pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    );
+    const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+
+    const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { days, hours, mins, secs };
+  }
 }
+
+function pad(value) {
+  return String(value).padStart(2, "0");
+}
+
+const timer = new CountdownTimer({
+  selector: "#timer-1",
+  targetDate: new Date("January 9, 2022, 14:00"),
+});
